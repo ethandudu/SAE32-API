c236528f54227e460a64e0886c9338d35a267674
Ethan <41381482+ethandudu@users.noreply.github.com>
2023-12-18 21:36:52 +0100

    Create mac.php
    Added mac vendor search for the android app

---
+++ mac.php
@@ -0,0 +1,72 @@
+<?php
+
+include ('config.php');
+header('Content-Type: application/json');
+
+//get the Authorization token from the header
+$headers = apache_request_headers();
+$tk = $headers['Authorization'];
+
+if (isset($_GET['macsrc'])) {
+    $macsrc = $_GET['macsrc'];
+}
+
+if (isset($_GET['macdst'])) {
+    $macdst = $_GET['macdst'];
+}
+
+//check if the token is correct
+if (isset($tk)){
+    if ($tk == $authorizedtoken){
+        if (isset($macdst) && isset($macsrc)){
+            $reqbdd = $bdd->prepare('SELECT Organization FROM oui WHERE Assignment = :macsrc');
+            $reqbdd->execute(array('macsrc' => $macsrc));
+            if ($reqbdd->rowCount() == 0){
+                $vendorsrc = 'Unknown';
+            } else {
+                $vendorsrc = $reqbdd->fetch();
+                $vendorsrc = $vendorsrc['Organization'];
+            }
+            $reqbdd->closeCursor();
+            $reqbdd = $bdd->prepare('SELECT Organization FROM oui WHERE Assignment = :macdst');
+            $reqbdd->execute(array('macdst' => $macdst));
+            if ($reqbdd->rowCount() == 0){
+                $vendordst = 'Unknown';
+            } else {
+                $vendordst = $reqbdd->fetch();
+                $vendordst = $vendordst['Organization'];
+            }
+            $reqbdd->closeCursor();
+            header("HTTP/1.1 200 OK");
+            $response = array(
+                'responsecode' => '200',
+                'message' => 'OK',
+                'vendorsrc' => $vendorsrc,
+                'vendordst' => $vendordst
+            );
+
+            
+        } else {
+            header("HTTP/1.1 200 OK");
+            $response = array(
+                'responsecode' => '410',
+                'message' => 'No mac address provided'
+            );
+        }
+    } else {
+        header("HTTP/1.1 401 Unauthorized");        
+        $response = array(
+            'responsecode' => '400',
+            'message' => 'Not authorized'
+        );
+    }
+} else {
+    header("HTTP/1.1 401 Unauthorized");
+    $response = array(
+        'responsecode' => '400',
+        'message' => 'Not authorized'
+    );
+}
+
+echo json_encode($response);
+?>
